43072fcae2202ae5f80175fe4c38f30d274f4022
oleh-shev <oshevchyck@gmail.com>
2021-06-08 00:31:09 +0300

    Complete game functionality

---
+++ components/Card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, StyleSheet} from "react-native";
+
+function Card(props) {
+  return (
+    <View style={{...styles.card, ...props.style}}>
+      {props.children}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    //Shadow iOS
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+
+    //Shadow Android
+    elevation: 8,
+
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+  }
+});
+
+export default Card;
